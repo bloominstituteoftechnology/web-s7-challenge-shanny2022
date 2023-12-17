48d4a368d0f89e992af068b085da58e988af2109
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-17 02:03:12 -0500

    update

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,41 +1,42 @@
 import React, { useState } from 'react';
 
-function Form() {
-  const [formData, setFormData] = useState({
-    fullName: '',
-    size: '',
-    toppings: []
-  });
+const initialFormData = {
+  fullName: '',
+  size: '',
+  toppings: []
+};
+
+const Form = () => {
+  const [formData, setFormData] = useState(initialFormData);
+  const [message, setMessage] = useState('');
 
   const handleChange = (event) => {
-    if (event.target.name === 'toppings') {
-      setFormData({
-        ...formData,
-        toppings: [...formData.toppings, event.target.value]
-      });
-    } else {
-      setFormData({
-        ...formData,
-        [event.target.name]: event.target.value
-      });
-    }
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Submit form data...
+    setMessage(`Thank you for your order, ${formData.fullName}!`);
+    setFormData(initialFormData);
+  };
+
+  const isFormValid = () => {
+    return formData.fullName && formData.size;
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Full Name:
-        <input type="text" id="fullName" name="fullName" value={formData.fullName} onChange={handleChange} />
+        <input type="text" name="fullName" value={formData.fullName} onChange={handleChange} />
       </label>
       <label>
         Size:
-        <select id="size" name="size" value={formData.size} onChange={handleChange}>
-          <option value="">--Please choose an option--</option>
+        <select name="size" value={formData.size} onChange={handleChange}>
+          <option value="">Select</option>
           <option value="S">S</option>
           <option value="M">M</option>
           <option value="L">L</option>
@@ -49,9 +50,10 @@ function Form() {
         <input type="checkbox" name="toppings" value="4" onChange={handleChange} /> Mushrooms
         <input type="checkbox" name="toppings" value="5" onChange={handleChange} /> Ham
       </label>
-      <input type="submit" value="Submit" />
+      <input type="submit" value="Submit" disabled={!isFormValid()} />
+      {message && <p>{message}</p>}
     </form>
   );
-}
+};
 
 export default Form;
--- mvpB.test.js
+++ mvpB.test.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Form from './Form';
+
+test('form submission shows success message', async () => {
+  render(<Form />);
+
+  test('Validation of `size` renders correct error message', async () => {
+    // ...test code...
+  }, 3000); // Increase timeout to 3000ms
+
+  // Fill out and submit the form...
+  const fullNameInput = screen.getByLabelText(/Full Name:/i);
+  fireEvent.change(fullNameInput, { target: { value: 'Fish' } });
+
+  const sizeSelect = screen.getByLabelText(/Size:/i);
+  fireEvent.change(sizeSelect, { target: { value: 'M' } });
+
+  const submitButton = screen.getByRole('button', { name: /Submit/i });
+  fireEvent.click(submitButton);
+
+  expect(await screen.findByText(/Thank you for your order, Fish!/i)).toBeInTheDocument();
+}, 3000); // Increase timeout to 3000ms
 
 describe('Form', () => {
   test('renders Form component', () => {
     render(<Form />);
-
-    expect(screen.getByLabelText(/Full Name:/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/Size:/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/Toppings:/i)).toBeInTheDocument();
-    expect(screen.getByText(/Submit/i)).toBeInTheDocument();
+    expect(screen.getByText(/Order Your Pizza/i)).toBeInTheDocument();
   });
 });
